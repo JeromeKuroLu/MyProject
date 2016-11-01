51db1ec2106da46538b7c42c41d2b55650d1ecd8
JeromeLu <arvinblureple@163.com>
2016-11-01 21:19:16 +0800

    reconstruct file structure

--- src/test/testInsertionSort.js
+++ src/test/testInsertionSort.js
@@ -1,7 +1,7 @@
 /**
  * Created by Jerome on 2016/9/7.
  */
-var insertionSort = require('../js/insertionSort');
+var insertionSort = require('../js/InsertionSort/insertionSort');
 
 var x = [12, 9, 22, 18, 11, 17];
 insertionSort(x);
