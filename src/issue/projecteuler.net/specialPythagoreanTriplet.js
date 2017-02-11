d9869b7ac79f75cc4e9908830b9c3e841e381f5c
JeromeLu <arvinblureple@163.com>
2017-02-11 18:28:31 +0800

    find the special Pythagorean Triplet product

---
+++ src/issue/projecteuler.net/specialPythagoreanTriplet.js
@@ -0,0 +1,42 @@
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ a^2 + b^2 = c2
+ For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ Find the product abc.
+
+ *  My middle formula b = (s^2 - 2as) / 2(s - a)
+ */
+
+function findPythagoreanTripletBySum(sum) {
+    var ra,
+        rb,
+        rc;
+    if (sum < 1) {
+        console.log("Please input available number.")
+    }
+    else {
+        var upperLimit = Math.floor(sum / 3);
+        for (var a = 3; a < upperLimit; a++) {
+            var b = (Math.pow(sum, 2) - 2 * a * sum) / (2 * (sum - a)),
+                ib = Math.floor(b);
+            if (ib === b && b > a) {
+                ra = a;
+                rb = b;
+                rc = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+                break;
+            }
+            else if (a == upperLimit - 1) {
+                console.log("There is no special Pythagorean Triplet.");
+            }
+        }
+    }
+    if (ra && rb && rc) {
+        console.log("The Pythagorean Triplet are: " + ra + ", " + rb + ", " + rc);
+        console.log("The Product is " + ra * rb * rc);
+    }
+}
+
+(function () {
+    findPythagoreanTripletBySum(1000);
+}());
